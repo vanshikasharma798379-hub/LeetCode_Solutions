02b47ee544c8685fa660344bfeb10b53d225c437
vanshikasharma798379-hub <vanshikasharma798379@gmail.com>
2026-07-15 12:00:05 +0530

    Create PeakIndexInMountainArray.java

---
+++ Binary_Search/PeakIndexInMountainArray.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int n = arr.length;
+        int low= 1 ;
+        int high = n-2;
+        while(low<=high)
+        {
+            int mid= (low+high)/2;
+            if(arr[mid] >arr[mid-1] && arr[mid]<arr[mid+1])
+            {
+                low = mid+1;
+            }
+            if(arr[mid] <arr[mid-1] && arr[mid]>arr[mid+1])
+            {
+                high = mid-1;
+            }
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
+            {
+                   return mid; 
+            }
+        }
+        return -1;
+        
+    }
+}
