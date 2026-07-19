7b87bb8209be57ac6439064e9cfcc92217b63772
vanshikasharma798379-hub <vanshikasharma798379@gmail.com>
2026-07-20 00:57:01 +0530

    Create GuessNumberHigherOrLower.java

---
+++ Binary_Search/GuessNumberHigherOrLower.java
@@ -0,0 +1,21 @@
+public class Solution extends GuessGame {
+    public int guessNumber(int n ) {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int result = guess(mid);
+            if (result == 0) {
+                return mid;
+            }
+            else if (result == -1) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+        
+    }
+}
