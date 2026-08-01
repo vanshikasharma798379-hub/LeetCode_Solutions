3d0a7d1c8266aa703ce85f7ee89969a2e4864ff4
vanshikasharma798379-hub <vanshikasharma798379@gmail.com>
2026-08-01 11:57:46 +0530

    Create Pascal'sTriangleII.java

---
+++ Arrays/Pascal'sTriangleII.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = rowIndex +1;
+        for(int i =0; i<n; i++)
+        {
+            ans.add(new ArrayList<Integer>());
+            for(int j =0 ; j<=i; j++)
+            {
+                if(j==0 || j==i)
+                {
+                    ans.get(i).add(1);
+                }
+                else
+                {
+                    ans.get(i).add(ans.get(i-1).get(j)+ans.get(i-1).get(j-1));
+                }
+            }
+        }
+        return ans.get(rowIndex);
+        
+    }
+}
