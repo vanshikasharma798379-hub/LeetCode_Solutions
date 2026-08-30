8bafc64f7ae1390bad7b7e75035de58549400cdd
vanshikasharma798379-hub <vanshikasharma798379@gmail.com>
2026-08-30 17:21:08 +0530

    Time: 0 ms (100%), Space: 44.6 MB (83.37%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int searchInsert(int[] arr, int target) {
+        int n = arr.length;
+        int low = 0;
+        int high = n-1;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(arr[mid] == target)
+            {
+                return mid;
+            }
+            else if(arr[mid]<target)
+            {
+                low = mid +1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+
+        }
+
+       return low;
+
+        
+    }
+}
