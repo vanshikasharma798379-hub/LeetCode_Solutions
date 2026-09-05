0f413f9a0972a8581cd220b149140931cee93a90
vanshikasharma798379-hub <vanshikasharma798379@gmail.com>
2026-09-05 21:38:41 +0530

    Time: 790 ms (9.28%), Space: 79.8 MB (14.31%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,11 +1,9 @@
 class Solution {
     public int countPrimes(int n) {
         boolean[] prime = new boolean[n];
-
         for (int i = 2; i < n; i++) {
             prime[i] = true;
         }
-
         for (int i = 2; i * i < n; i++) {
             if (prime[i]) {
                 for (int j = i * i; j < n; j += i) {
@@ -13,15 +11,12 @@ class Solution {
                 }
             }
         }
-
         int count = 0;
-
         for (int i = 2; i < n; i++) {
             if (prime[i]) {
                 count++;
             }
         }
-
         return count;
     }
 }
